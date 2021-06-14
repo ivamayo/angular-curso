f3f0233a84c881506dada368e9fd858424c15ef7
Ivan Mayo <ivanmayo1785@gmail.com>
2021-06-14 16:57:38 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// import { ContadorComponent } from './contador/contador/contador.component';
 
+//Crear un nuevo modulo de contador  ContadorModule
+//declaraciones, y exportaciones
+
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // ContadorComponent,
+    // HeroeComponent,
+    // ListadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
